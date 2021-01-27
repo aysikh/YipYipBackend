d0cec277daad8d12cc1d2c1318234c14bbad5a73
theprivatepark <anneypark@gmail.com>
2021-01-27 16:09:34 -0500

    fixed models comment to content

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :yipper
 
-  validates :comment, length: { maximum: 240 }
-  
+  validates :content, length: { maximum: 240 }
+
 end
--- app/models/yipper.rb
+++ app/models/yipper.rb
@@ -2,7 +2,7 @@ class Yipper < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :comment, presence: true
-  validates :comment, length: { maximum: 240 }
+  validates :content, presence: true
+  validates :content, length: { maximum: 240 }
 
 end
