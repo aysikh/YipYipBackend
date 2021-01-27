0359cff015a375e4ace4d83be128a4e91c5cdff0
aysikh <amy.sikhammountry@gmail.com>
2021-01-27 15:07:23 -0500

    added functions to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,37 @@
 class CommentsController < ApplicationController
+  before_action :find_comment, only [:show]
+
+  def index
+    @comments = Comment.all 
+    render json: @comments
+  end
+
+  def show
+  end
+
+  def create
+    @user = User.find(:user_id)
+    @yipper = Yipper.find(:yipper_id)
+    if @comment.save
+      render json: {
+        comment: @comment, 
+        errors: false
+      }
+    else
+      render json: {
+        errors: true, 
+        info: ["comment could not be posted"]
+      }
+  end
+
+  private
+
+  def find_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :yipper_id)
+
+
 end
--- app/controllers/yippers_controller.rb
+++ app/controllers/yippers_controller.rb
@@ -1,5 +1,5 @@
 class YippersController < ApplicationController
-  before_action :find_yipper, only [:show, :edit]
+  before_action :find_yipper, only [:show]
 
   def index
     @yippers = Yipper.all
