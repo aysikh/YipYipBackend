082a37b1d2c1d11b33ab5a160dc457010620900a
theprivatepark <anneypark@gmail.com>
2021-01-27 15:45:28 -0500

    added validations and relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :yippers
+  has_many :comments
+  belongs_to :location
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :location_id, presence: true
+
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+
 end
--- app/models/yipper.rb
+++ app/models/yipper.rb
@@ -1,2 +1,8 @@
 class Yipper < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+
+  validates :comment, presence: true
+  validates :comment, length: { maximum: 240 }
+
 end
