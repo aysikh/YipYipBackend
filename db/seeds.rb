13406e3838a93f5e85f41016ca1ab17f75f60ebb
theprivatepark <anneypark@gmail.com>
2021-01-27 15:59:03 -0500

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+Location.destroy_all
+Yipper.destroy_all
+
+fairfaxva = Location.create(city: "Fairfax, VA")
+
+amy = User.create(email: "amy@gmail.com", username: "aymes", name: "Amy", password_digest: "nugget", location_id: fairfaxva.id)
+anney = User.create(email: "anney@gmail.com", username: "theprivatepark", name: "Anney", password_digest: "james", location_id: fairfaxva.id)
+mark = User.create(email: "mark@gmail.com", username: "navak", name: "Mark", password_digest: "wow", location_id: fairfaxva.id)
+
+Yipper.create(content: "I need a job", vote: 69, user_id: amy.id)
+Yipper.create(content: "Someone hire me", vote: 420, user_id: anney.id)
+Yipper.create(content: "Job search fucking sucks", vote: 666, user_id: mark.id)
+
+
